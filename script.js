93533a510db0b7d2078fd6a30057aefb997ab840
geethika-antony <geethikaa28@gmail.com>
2025-06-29 18:05:15 +0530

    feat: testing different types of inputs

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const add = require("../string-calculator/src/stringCalculator");
 
-console.log(add(""));  // 0
-console.log(add("5")); // 5
-console.log(add("2,4")); // 6 
-console.log(add("5,6,1,3")); // 15
-console.log(add("1\n2,3")); // 6 //newline
-console.log(add("//;\n1;2;4")) // 7 //custom delimiter
+console.log(add(""));  //0
+console.log(add("5")); //5
+console.log(add("2,4")); //6 
+console.log(add("5,6,1,3")); //15
+console.log(add("1\n2,3")); //6 //newline
+console.log(add("//;\n1;2;4")) //7 //custom delimiter
 //console.log(add("-1,2")); //should throw error message
 //console.log(add("-1,2,-3,-4,5,6")); //should throw error message
+console.log(add("2,,4")); //6
+console.log(add("2,4,")); //6
+console.log(add("1\n")); //1
+console.log(add(" ")); //0
+console.log(add("//;;\n1;;2;;3")); //6
